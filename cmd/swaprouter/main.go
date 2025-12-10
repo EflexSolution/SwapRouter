2a600a3116b3486b503d2efb994560c2aa49aebf
Efle <152645371+EflexSolution@users.noreply.github.com>
2025-12-10 19:13:53 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/swaprouter/main.go
@@ -0,0 +1,23 @@
+// cmd/swaprouter/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "swaprouter/internal/swaprouter"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := swaprouter.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
